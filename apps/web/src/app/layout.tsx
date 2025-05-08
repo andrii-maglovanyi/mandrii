9368096c8e513a0362504a987402ff56dfc8f407
Andrii Maglovanyi <andrii.maglovanyi@vonage.com>
2025-05-08 22:06:02 +0100

    fix

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <img src="" />
+      <img src="d" />
       <body>{children}</body>
     </html>
   );
