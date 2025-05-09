b406929105f4ddc30d4a07fd298e5a9b97c8b5f7
Andrii Maglovanyi <andrii.maglovanyi@vonage.com>
2025-05-09 06:47:00 +0100

    fix

--- apps/web/.lintstagedrc.mjs
+++ apps/web/.lintstagedrc.mjs
@@ -1,6 +1,12 @@
 import baseConfig from "../../.lintstagedrc.base.mjs";
+import path from "path";
+
+const buildEslintCommand = (filenames) =>
+  `pnpm lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
 
 export default {
   ...baseConfig,
-  // "*.{ts,tsx}": ["next lint --fix"],
+  "*.{ts,tsx}": [buildEslintCommand],
 };
