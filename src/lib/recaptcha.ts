143af89d3eabbd948addc27abdcb3391ba3a96c8
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2025-08-21 14:29:14 +0100

    pass automation bypass header

--- src/lib/recaptcha.ts
+++ src/lib/recaptcha.ts
@@ -1,9 +1,15 @@
+import { isPreview } from "./config/env";
 import { UrlHelper } from "./url-helper";
 
 export async function validateCaptcha(token: string, action: string): Promise<boolean> {
   const res = await fetch(`${UrlHelper.buildApiUrl("captcha")}`, {
     body: JSON.stringify({ action, token }),
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      ...(isPreview && process.env.VERCEL_AUTOMATION_BYPASS_SECRET
+        ? { "x-vercel-protection-bypass": process.env.VERCEL_AUTOMATION_BYPASS_SECRET }
+        : {}),
+    },
     method: "POST",
   });
 
