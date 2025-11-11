a0ccf10bb1bbb5389048f26d5b37e532ab1986c8
Andrii Maglovanyi <andrii.maglovanyi@gmail.com>
2025-11-11 08:00:19 +0000

    fix: minor

--- apps/web/src/features/Events/EventCard/Components/CardHeader.tsx
+++ apps/web/src/features/Events/EventCard/Components/CardHeader.tsx
@@ -83,7 +83,10 @@ export const CardHeader = ({ event, hideUntilHover = false }: CardHeaderProps) =
           <ActionButton
             aria-label={i18n("Manage event")}
             className="group"
-            icon={<PenTool className={hideUntilHover ? `hidden group-hover/card:flex` : ""} size={18} />}
+            icon={<PenTool className={hideUntilHover ? `
+              hidden
+              group-hover/card:flex
+            ` : ""} size={18} />}
             onClick={handleManageClick}
             size="sm"
             variant="ghost"
@@ -92,14 +95,20 @@ export const CardHeader = ({ event, hideUntilHover = false }: CardHeaderProps) =
         <ActionButton
           aria-label={i18n("Share this event")}
           className="group"
-          icon={<Share2 className={hideUntilHover ? `hidden group-hover/card:flex` : ""} size={18} />}
+          icon={<Share2 className={hideUntilHover ? `
+            hidden
+            group-hover/card:flex
+          ` : ""} size={18} />}
           onClick={handleShareClick}
           size="sm"
           variant="ghost"
         />
         {Boolean(event.owner_id) && (
           <Tooltip label={i18n("Verified event")} position="left">
-            <BadgeCheck className={`stroke-green-600 dark:stroke-green-400`} />
+            <BadgeCheck className={`
+              stroke-green-600
+              dark:stroke-green-400
+            `} />
           </Tooltip>
         )}
       </div>
@@ -108,7 +117,9 @@ export const CardHeader = ({ event, hideUntilHover = false }: CardHeaderProps) =
 
   return (
     <div className="mb-2 flex h-8 justify-between gap-2">
-      <div className={`text-on-surface flex h-full min-w-0 flex-1 items-center gap-1 text-sm`}>
+      <div className={`
+        flex h-full min-w-0 flex-1 items-center gap-1 text-sm text-on-surface
+      `}>
         {getIcon(iconName, { size: 18 })}
         <span className="block min-w-0 flex-1 truncate">{label[locale]}</span>
 
